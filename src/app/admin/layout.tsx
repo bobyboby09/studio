8ebc69167ef3c754225d5d6093ee955345fadbf4
Kuldeep kumar all type videos <kuldeepkumardance24@gmail.com>
2025-08-04 21:13:27 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -21,7 +21,7 @@ export default function AdminLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased bg-background">
+      <body className="font-body antialiased">
         {children}
         <Toaster />
       </body>
